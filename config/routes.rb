9c6bfb616bbce02d43b47f56a76ed79a7a000429
unknown <kumampet@gmail.com>
2019-12-09 05:55:58 +0900

    Create Diary routine

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'contents/music'
   get '/' => 'home#top' #トップページはアドレスのルートからアクセスできるようにする
   get '/about' => 'home#about'
+
+  get '/diary' => 'diaries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
